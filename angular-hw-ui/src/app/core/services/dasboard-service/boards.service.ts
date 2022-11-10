3bb1b5791799871e807406baa2e81726fc6cfdea
Nadiia_Matura <manb@ukr.net>
2022-11-10 17:56:18 +0200

    13

--- angular-hw-ui/src/app/core/services/dasboard-service/boards.service.ts
+++ angular-hw-ui/src/app/core/services/dasboard-service/boards.service.ts
@@ -18,8 +18,15 @@ export class BoardsService {
     return this.boards;
   }
 
-  getBoardByName() {
-
+  getBoardByName(boardsKey: string, boardName: string) {
+    let boardsFromLocalStorage = localStorage.getItem(boardsKey);
+    if (boardsFromLocalStorage) {
+      this.boards = JSON.parse(boardsFromLocalStorage);
+    } else this.boards = [];
+    let board: any = this.boards.find((item: any) => item.name === boardName);
+    this.boards = [];
+    this.boards.push(board);
+    return this.boards;
   }
 
   addNewBoard(boardsKey: string, board: any) {
@@ -32,7 +39,7 @@ export class BoardsService {
     return this.boards;
   }
 
-  editBoardName(boardsKey: string, boardId: number, newBoardName: string) {
+  editBoardName(boardsKey: string, boardId: any, newBoardName: string) {
     let boardsFromLocalStorage = localStorage.getItem(boardsKey);
     if (boardsFromLocalStorage) {
       this.boards = JSON.parse(boardsFromLocalStorage);
@@ -43,6 +50,8 @@ export class BoardsService {
         board.name = newBoardName;
       }
     }
+    localStorage.setItem(boardsKey, JSON.stringify(this.boards));
+    return this.boards;
   }
 
   deleteBoard(boardsKey: string, board: { name: any; }): any {
@@ -56,9 +65,18 @@ export class BoardsService {
     return this.boards;
   }
 
-  updateNumberOfTasks() {
-
+  updateNumberOfTasks(boardsKey: string, boardId: any, newNumberOfTasks: number) {
+    let boardsFromLocalStorage = localStorage.getItem(boardsKey);
+    if (boardsFromLocalStorage) {
+      this.boards = JSON.parse(boardsFromLocalStorage);
+    } else this.boards = [];
+    let board: any;
+    for (board in this.boards) {
+      if (board.boardId === boardId) {
+        board.numberOfTasks = newNumberOfTasks;
+      }
+    }
+    localStorage.setItem(boardsKey, JSON.stringify(this.boards));
+    return this.boards;
   }
-
-
 }
